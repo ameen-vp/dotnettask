435168fc1e37e714dc4dd7772854d23af39e0c0c
ameena <muhammedameenvpa@gmail.com>
2025-03-12 22:10:08 +0530

    primenumber

---
+++ primenumber/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class PrimeNumber
+{
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+            return false;
+
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (IsPrime(num))
+            Console.WriteLine($"{num} is a prime number.");
+        else
+            Console.WriteLine($"{num} is not a prime number.");
+    }
+}
